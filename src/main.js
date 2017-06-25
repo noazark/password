54693b740f39674a22fb68af1971f0bc0d7f6c02
Noah H. Smith <noah@batteredoats.com>
2017-06-25 16:22:27 -0700

    installed raven for exception tracking

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App'
 import VueAnalytics from 'vue-analytics'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
 
 Vue.config.productionTip = false
 
 const store = {
   ready: false
 }
+
+Raven
+  .config('https://428a62fb569045aebf3944b61b4724f4@sentry.io/183657')
+  .addPlugin(RavenVue, Vue)
+  .install()
 
 Vue.use(VueAnalytics, {
   id: 'UA-78783837-2',
